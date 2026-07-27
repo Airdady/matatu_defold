52685b82d587e09ca4722ee0bf9746d23e6092e2
matatu-league <matatu.league@gmail.com>
2026-07-27 21:11:59 +0300

    improve

--- modules/config.lua
+++ modules/config.lua
@@ -8,9 +8,9 @@ local is_web = sys.get_sys_info().system_name == "HTML5"
 
 -- If in a browser, talk to localhost. If on Android/Mac, use the network IP.
 if is_web then
-    M.DOMAIN = "champion.matatuleague.com"
+    M.DOMAIN = "api.matatuleague.com"
 else
-    M.DOMAIN = "champion.matatuleague.com"
+    M.DOMAIN = "api.matatuleague.com"
 end
 
 -- Endpoints follow the active game (see modules/game_mode.lua). The backend
